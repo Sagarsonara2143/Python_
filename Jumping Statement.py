d8122789ccf8b52889abbd7c906e64239c514a13
Sagarsonara2143 <sagarsonara2143@gmail.com>
2023-03-13 14:01:10 +0530

    13.03.2023

---
+++ Jumping Statement.py
@@ -0,0 +1,17 @@
+''' Continue and Break keyword used to Jump loop '''
+
+
+print("Example for Continue")
+for i in range(1,11):
+    if i == 4:
+        continue
+    else:
+        print(i)
+
+
+print("Example for Break")
+for i in range(1,11):
+    if i == 4:
+        break
+    else:
+        print(i)
---
+++ factorial.py
@@ -0,0 +1,5 @@
+import math
+
+a = int(input("Enter Value of A: "))
+
+print(math.factorial(a))
