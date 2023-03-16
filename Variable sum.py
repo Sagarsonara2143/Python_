e763d963ff213db67024f9de058dd82e2324aa4e
Sagarsonara2143 <sagarsonara2143@gmail.com>
2023-03-16 22:18:00 +0530

    16.03.2023

---
+++ Variable sum.py
@@ -0,0 +1,8 @@
+a = int(input("Enter Value of A: "))
+b = int(input("Enter Value of B: "))
+
+c = a+b
+print("Sum of two int is : ",c)
+print("Differnce of two int is : ", a-b)
+
+print("Multification of two int is : ",a*b)
