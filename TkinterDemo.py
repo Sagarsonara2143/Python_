f6744b6dfc4bff5fb04e6960b772902e15de3a3d
Sagarsonara2143 <sagarsonara2143@gmail.com>
2023-03-25 21:57:48 +0530

    25.03.2023

--- TkinterDemo.py
+++ TkinterDemo.py
@@ -3,3 +3,22 @@ from tkinter import *
 root = Tk()                # it will open blank page
 root.geometry("500x500")    # it will open page in size of 500 X 500
 root.title("My First Tkinter Example")
+
+
+
+#Create a Label and place in root
+
+l_id = Label(root,text="ID",font=("Arial",15))
+l_id.place(x = 50, y=50)
+
+l_fname = Label(root,text="Fname",font=("Arial",15))
+l_fname.place(x = 50, y=100)
+
+l_lname = Label(root,text="Lname",font=("Arial",15))
+l_lname.place(x = 50, y=150)
+
+l_email = Label(root,text="Email",font=("Arial",15))
+l_email.place(x = 50, y=200)
+
+l_mobile = Label(root,text="Mobile",font=("Arial",15))
+l_mobile.place(x = 50, y=250)
