c825e6f6ca5971c6db917dc2375b509fde6ffe67
Sagarsonara2143 <sagarsonara2143@gmail.com>
2023-03-28 21:15:31 +0530

    28.03.2023

--- TkinterDemo.py
+++ TkinterDemo.py
@@ -61,6 +61,27 @@ def search_data():
             msg.showinfo("Search Status","Opps..! ID is not found")
         con.close()
 
+# Update data
+def update_data():
+    if e_id.get()=="" or e_fname.get()=="" or e_lname.get()=="" or e_email.get()=="" or e_mobile=="":
+        msg.showinfo("Update Status","All fields are Mandatory")
+    else:
+        con = create_con()
+        cursor=con.cursor()
+        query="update student set fname=%s, lname=%s, email=%s,mobile=%s where id=%s"
+        args = (e_fname.get(),e_lname.get(),e_email.get(),e_mobile.get(),e_id.get())
+        cursor.execute(query,args)
+        con.commit()
+        con.close()
+        e_id.delete(0,"end")
+        e_fname.delete(0,"end")
+        e_lname.delete(0,"end")
+        e_email.delete(0,"end")
+        e_mobile.delete(0,"end")
+        msg.showinfo("Update Status","Data Updated Successfully")
+        
+       
+
         
 root = Tk()                                                                             # it will open blank page
 root.geometry("400x400")                                                    # it will open page in size of 400 X 400
@@ -112,7 +133,7 @@ insert.place(x=50,y=300)
 search=Button(root,text="SEARCH",font=("Arial",11),fg="White",bg="Black",command = search_data)
 search.place(x=120,y=300)
 
-update=Button(root,text="UPDATE",font=("Arial",11),fg="White",bg="Black")
+update=Button(root,text="UPDATE",font=("Arial",11),fg="White",bg="Black",command=update_data)
 update.place(x=197,y=300)
 
 delete=Button(root,text="DELETE",font=("Arial",11),fg="White",bg="Black")
