9d10ea9f2b5f5d400c6417203f827c609b615549
Sagarsonara2143 <sagarsonara2143@gmail.com>
2023-05-27 21:48:43 +0530

    27.05.2023

---
+++ Interview.py
@@ -0,0 +1,42 @@
+import datetime
+from datetime import date
+
+'''
+n = int(input("Enter N Number : "))
+list = [0, 1]
+[list.append(sum(list[-2:])) for x in range(n)]
+print(list[:n])
+'''
+'''
+today=datetime.date.today()
+yesterday=today - datetime.timedelta(days=1)
+tommorow=today + datetime.timedelta(days=1)
+
+
+print("Yesterday : ",yesterday.strftime("%d-%m-%Y"))
+print("Today : ",today.strftime("%d-%m-%Y"))
+print("Tommorow : ",tommorow.strftime("%d-%m-%Y"))
+'''
+
+#   x,y=10,20
+#   x,y=y,x
+#   print("After Swap  X is ",x,"and Y is ",y )
+
+f=open("interview.txt","w")
+f.write("This is a demo of File management in Python")
+f.close()
+
+f=open("interview.txt","r")
+print(f.read())
+f.close()
+print("*"*80)
+
+f=open("interview.txt","a")
+f.write("\nThis is Appended Text")
+f.close()
+
+f=open("interview.txt","r")
+print(f.read())
+f.close()
+print("*"*80)
+
