766af69d9f61b8b8b02050753658f30627bfc77d
Sagarsonara2143 <sagarsonara2143@gmail.com>
2023-05-28 12:43:59 +0530

    27.05.2023

---
+++ pandas.py
@@ -0,0 +1,22 @@
+import pandas as pd
+
+
+data={
+    'student':['sagar','sanjay','Ansh'],
+    'Marks':[95,97,88],
+    'Grade':["A+","A+","A"]
+    }
+
+var=pd.DataFrame(data)              # Data Display in Table like row and column
+print(var)                                      # Key Value is Display in First Row
+
+
+
+a=[1,7,5]                                       
+print(pd.Series(a))                     # Series will display data in column
+print(pd.Series(a)[2])                  # Result of second index column - Ans 5 
+
+a=[10,15,20]
+var=pd.Series(a,index=["a","b","c"])        # Index is with arguments display labels in index - By default value of index is start from 0
+print(var)      
+print("Value of B Index : ",var["b"])       # We can find out data with index value
